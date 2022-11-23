54be23664ed3d52a9f8660dcc79256e93cfd8742
Mandar Swami <mandarswami22@gmail.com>
2022-11-23 15:24:45 +0530

    added new info

--- src/class.h
+++ src/class.h
@@ -7,6 +7,34 @@
 
 using namespace std;
 
+class Calculator;
+class Expression;
+
+class Operation;
+
+// TODO :
+// Define below classes and its methods accordingly in a new file
+// like Addition.cpp, Subtractin.cpp etc
+// These classes will be child classes of Operation
+class Addition;
+class Subtraction;
+class Multiplication;
+class Division;
+class Percentage;
+class SquareRoot;
+
+class Operation
+{
+protected:
+    double a;
+    double b;
+
+public:
+    Operation(double x, double y) : a(x), b(y) {} // constructor
+    double solve() {}                             // each operation will return its result after the calculation
+    void printOP() {}
+};
+
 void welcome();
 
 #endif
